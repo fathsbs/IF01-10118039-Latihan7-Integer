9f7bd6ebf94e6d61fd6cbb51de5ab05e42a35875
fathsbs <55565303+fathsbs@users.noreply.github.com>
2019-09-23 20:08:30 +0700

    Add files via upload

---
+++ IF01-10118039-Latihan7-Integer/src/IF0110118039Latihan7Integer.java
@@ -0,0 +1,28 @@
+/*
+ * NAMA              : FATAHILLAH SATRIA BIMA SENO
+ * KELAS             : IF-01
+ * NIM               : 10118039
+ * DESKRIPSI PROGRAM :   
+ */
+
+public class IF0110118039Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+    }
+
+}
